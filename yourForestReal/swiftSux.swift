90523d733bb4457119b6b14ce7a032354a6c26b2
Karsten Wennerlund <kandw15@gmail.com>
2024-07-01 14:31:33 -0500

    Karsten first push

---
+++ yourForestReal/swiftSux.swift
@@ -0,0 +1,18 @@
+//
+//  swiftSux.swift
+//  yourForestReal
+//
+//  Created by Karsten Wennerlund on 7/1/24.
+//
+
+import SwiftUI
+
+struct swiftSux: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    swiftSux()
+}
