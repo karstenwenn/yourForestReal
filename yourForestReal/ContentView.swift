14c3c080ad842a4fea89383d2dad223ede693396
Karsten Wennerlund <kandw15@gmail.com>
2024-07-01 14:25:53 -0500

    Initial Commit

---
+++ yourForestReal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  yourForestReal
+//
+//  Created by Karsten Wennerlund on 7/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ yourForestReal/yourForestRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  yourForestRealApp.swift
+//  yourForestReal
+//
+//  Created by Karsten Wennerlund on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct yourForestRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
